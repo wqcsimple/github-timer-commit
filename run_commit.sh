928e48968d6faad7bba30d03fd10e2d2b885d3cd
whis <wqcsimple@foxmail.com>
2017-06-01 12:30:23 +0800

    init

---
+++ run_commit.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DATE=`date '+%Y-%m-%d'`
+TIME=`date '+%H:%M:%S'`
+
+COMMIT_TIME=${DATE}" "${TIME}
+
+echo "${COMMIT_TIME} commit \r\n" >> ./RADNE.MD
+
+branch_list=$(git branch | grep '*')
+current_branch=${branch_list:2}
+
+git status && git add -A && git commit -m "${COMMIT_TIME} commit" && git push origin ${current_branch}
